1dbdaa6865a64c1240921988e11009f3bb9c2c22
MarkTang <marktangwao@foxmail.com>
2016-05-14 20:08:04 +0800

    Test first github.

---
+++ ContosoUniversity/AAA/First.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContosoUniversity.AAA
+{
+    public class First
+    {
+        public void GetSum(int a, int b)
+        {
+            int c = a + b;
+        }
+    }
+}
