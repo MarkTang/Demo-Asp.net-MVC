22c4a11a798f56156db474c31e7cdf4cc9b5d7ec
MarkTang <marktangwao@foxmail.com>
2016-05-14 21:53:55 +0800

    Modifty product data on index page.

--- ContosoUniversity/App_Start/WebApiConfig.cs
+++ ContosoUniversity/App_Start/WebApiConfig.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web.Http;
+﻿using System.Web.Http;
 
 namespace ContosoUniversity
 {
@@ -11,6 +8,10 @@ namespace ContosoUniversity
         {
             // Web API configuration and services
 
+            //// Configure Web API to use only bearer token authentication.
+            //config.SuppressDefaultHostAuthentication();
+            //config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
+
             // Web API routes
             config.MapHttpAttributeRoutes();
 
@@ -19,6 +20,11 @@ namespace ContosoUniversity
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            GlobalConfiguration.Configuration.Formatters.XmlFormatter.SupportedMediaTypes.Clear();
+            var json = GlobalConfiguration.Configuration.Formatters.JsonFormatter;
+            json.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.None;
+
         }
     }
 }
